9ac0e7f5646f080fc033551aa98081182b046d41
bay007 <tnt.galicia@gmail.com>
2017-01-12 03:37:10 -0600

    generacion de los eventos necesarios para que el usuario pueda aumentar en 1 el valor del contador cada que toca la pantalla, en un toque largo se resetea el contador, mejora-> dividir la pantalla de tal suerte que se puedan llevar 2 cuentas a la vez, haciendo que cada divicion tenga como padre a un mismo componente

--- src/mainView.js
+++ src/mainView.js
@@ -4,45 +4,81 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TouchableOpacity ,
+  View,
+  Vibration,
+  Alert
 } from 'react-native';
 
 export default class contacosas extends Component {
+constructor(props) { //declaramos el constructor de el componente (NOTA: La estructura del constructor es una estructura bien definida)
+    super(props);
+    
+    this.state = {
+        inputValue: 0 //inicializamos la variable que usaremos para llevar la cuenta
+    }
+}
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double taps R on your keyboard to reload,{'\n'}
-          Shake or press menu button sfor dev menu
-        </Text>
+      <View style={styles.mainContainer}>
+        {/*definimos el espacio necesario para que el usuario pueda interactuar con TouchableOpacity*/}
+      {/*definimos las funciones ECM6 _onLongPress y _onPress*/}
+        <TouchableOpacity onLongPress={this._onLongPress} onPress={this._onPress} style={styles.buttonContainer}>
+          <View>
+            {/*mostramos con this.state.inputValue el valor del conteo*/}
+            <Text style={styles.buttonContainerText}>{this.state.inputValue}</Text>
+          </View>
+        </TouchableOpacity >
       </View>
+    )
+  }
+
+  _onLongPress = () =>{ //se define el comportamiento, en este caso, el uso de alert.
+    Alert.alert(
+      'Reset',
+      'Se regresa el valor a 0',
+      [
+        {
+          text: 'OK', onPress: this.setState({ inputValue: 0 })
+        },
+      ],
+      {
+        cancelable: true
+      }
     );
   }
+
+  _onPress = () =>{ //definir el comportamiento de lo que hará la aplicacion al tocarla normal NOTA: Para usar VIBRATE hay que dar permisos en el archivo manifest y volver a compilar
+    this.setState(
+      {
+      inputValue: this.state.inputValue+1 //aumentamos el valor de lo que se despliega en pantalla en 1
+      }
+    );
+    Vibration.vibrate([0,150]); //delay de 0 y luego dura 150
+  }  
+
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  buttonContainer:{
+    flex: 50,
+    opacity:0.9,
+    backgroundColor: '#3a53c1',
+    borderWidth: 2,
+    borderColor: '#91AA9D',
+    justifyContent: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  buttonContainerText:{
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlign: 'center'
+    }
 });
 
 AppRegistry.registerComponent('contacosas', () => contacosas);
